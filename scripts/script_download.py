8c283cb5ca6bf9f278f2cbc0bdddfec820d28b1f
Ashen-Elish <118285128+Ashen-Elish@users.noreply.github.com>
2024-01-06 01:14:19 +0500

    Update script_download.py

--- scripts/script_download.py
+++ scripts/script_download.py
@@ -1,1 +1,17 @@
+# Скрипт для загрузки датасета
+
+import pandas as pd
+import dvc.api
+
+# Загрузить датасет из удаленного хранилища с помощью dvc
+with dvc.api.open(
+    path="data/insurance.csv",
+    repo="https://github.com/user/repo.git",
+    mode="r",
+    remote="s3://bucket/path"
+) as fd:
+    df = pd.read_csv(fd)
+
+# Сохранить датасет в локальный файл
+df.to_csv("data/insurance.csv", index=False)
 
